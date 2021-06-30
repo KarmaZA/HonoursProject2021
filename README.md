8d37e06084f27baee39c202c3f5d4c37deb6b05c
KarmaZA <EVRJON003@myuct.ac.za>
2021-06-30 23:00:27 +0200

    Initial commit
    Added a bunch of basics for the projects. Mostly just to get it started and to get the ball rolling.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HonoursProject2021
 This is my Honours project that will make use of image matching to determine planting patterns in images of crops and extract data about their parameters.
+
+This is a project for the company Aerobotics and will use Python to develop this application.
---
+++ Scripts/main.py
@@ -0,0 +1,17 @@
+# Developed By Jonathon Everatt
+import numpy as np
+import scipy
+
+
+def main():
+    print("First function called.")
+
+
+def loadImageFromFile():
+    print("Loading the image file.")
+
+
+if __name__ == '__main__':
+    print("The program has started.")
+    loadImageFromFile()
+    main()
