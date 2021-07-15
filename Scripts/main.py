1e77e7a0c59e4d18d936e886778de6ca1b8281d2
KarmaZA <EVRJON003@myuct.ac.za>
2021-07-15 11:04:37 +0200

    Loaded image into program

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,6 +1,8 @@
 # Developed By Jonathon Everatt
-import numpy as np
-import scipy
+
+import numpy #Import Numpt
+import cv2 #Import OpenCV
+display_image_on_load = False
 
 
 def main():
@@ -9,9 +11,23 @@ def main():
 
 def loadImageFromFile():
     print("Loading the image file.")
+    source_image = cv2.imread('695787.png', 1)
+    if not source_image.any():
+        print("Error image not loaded")
+
+    cv2.imshow('image', source_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    print("Image loaded.")
+
 
 
 if __name__ == '__main__':
     print("The program has started.")
+
+    flagCheck = input("Do you want to see the image loaded (Y/N)?\n0")
+    if flagCheck == 'Y':
+        display_image_on_load = True
+
     loadImageFromFile()
     main()
