2208600f68ccb54bacc106921088cc2bb9048dea
Jonathon Everatt <everatt.jonathon@gmail.com>
2021-08-02 16:37:22 +0200

    This is better now
    Using the correct library to make this work

--- Scripts/importData.py
+++ Scripts/importData.py
@@ -1,9 +1,20 @@
 # File for importing the Data from files
 # Also will be used for creating and manipulating data structures
-import json
+from logging import error
+import geopandas as gpd
 
-# 
+# Using Geopandas as it is updated more frequently
 def importGeoJSONData(filename):
-    with open(filename) as f:
-        data = json.loads(f.read())
-    return data
+    try:
+        with open(filename) as f:
+        #data is a pandas.dataframe variable type
+            data = gpd.read_file(f)
+      
+        # Note use of iterrows() to iterate   
+        # for element, row in data.iterrows():
+        #    print(element, row)
+        #print(data)
+        return data
+    except:
+        print("File not found")
+        return error
