635a5505e8a0adde4d6be454af0765ece93ff353
Jonathon Everatt <everatt.jonathon@gmail.com>
2021-08-10 21:20:31 +0200

    Working for square on idealised data. need to show number of squares and then post output

--- Scripts/genImages.py
+++ Scripts/genImages.py
@@ -3,19 +3,22 @@
 import numpy as np
 import imageio
 
+
+Template_Square = [[0,0],[0,1],[1,0],[1,1]]
+
 def genImageIdealised(PointSet):
     width = 512
     height = 512
     nrChannels = 3
     ImageToGen = np.zeros(shape=[height, width, nrChannels], dtype=np.uint8)
-    imageio.imsave('template.png', ImageToGen)
+    imageio.imsave('MainImage.png', ImageToGen)
     
-    imageArray = imageio.imread('template.png')
+    imageArray = imageio.imread('MainImage.png')
     print(type(imageArray))
     print(ImageToGen.shape, ImageToGen.dtype)
     
     # Create a .raw file for the image
-    ImageToGen.tofile('template.raw')
+    ImageToGen.tofile('MainImage.raw')
     
     # Insert distance calculation or number assignment
     
@@ -28,5 +31,15 @@ def genImageIdealised(PointSet):
         #ImageToGen[point.x*10][point.y+10][1] = 255
         #ImageToGen[point.x*10][point.y+10][2] = 255
         
-    imageio.imsave('template.png', ImageToGen)
+    imageio.imsave('MainImage.png', ImageToGen)
+    
+    # Generate Template
+    # Currently Set for the Square Template
+    TemplateToGen = np.zeros(shape=[33,33,3], dtype=np.uint8)
+    for Template_Points in Template_Square:
+        x = 1 + Template_Points[0] * 30
+        y = 1 + Template_Points[1] * 30
+        TemplateToGen[x][y][0:3] = 255
+        
+    imageio.imsave('TemplateImage.png', TemplateToGen)
     
--- Scripts/main.py
+++ Scripts/main.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     testVar = importData.importGeoPandasJSon('Test36507.geojson')
     
     testPointSet = importData.importGeoJSonAsPoints('Test36507.geojson')
-    testPointSet = importData.importIdealisedData('TriIdeal3.txt')
+    testPointSet = importData.importIdealisedData('SquareIdeal3.txt')
     print("Drawing the image call on main")
     genImages.genImageIdealised(testPointSet)
     # importData.displayPointSet(testPointSet)
@@ -52,12 +52,12 @@ if __name__ == '__main__':
 
     file_input_name = input("What is the image name(0 for default)?\n")
     if file_input_name == '0':
-        file_input_name = '695787.png'
+        file_input_name = 'MainImage.png'
         print("Using default")
 
     source_image = loadImageFromFile(file_input_name)
     print("Image loaded")
-    template_image = loadImageFromFile('Template695.png')
+    template_image = loadImageFromFile('TemplateImage.png')
     print("Template loaded")
     # SimilarityMeasures.correlation_measure = templateMatching_leastSquares(source_image, template_image)
     SimilarityMeasures.templateMatching_leastSquares(source_image, template_image)
