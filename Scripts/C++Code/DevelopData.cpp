9689c872a587a53745896493cda404e870ca7185
Jonathon Everatt <everatt.jonathon@gmail.com>
2021-08-19 11:27:18 +0200

    Made double hedgerow data and the DevelopData is how to gen it

--- Scripts/C++Code/DevelopData.cpp
+++ Scripts/C++Code/DevelopData.cpp
@@ -14,28 +14,33 @@ using namespace std;
 int main(){
 	int count = 0;
 	double toReturn[181][2];
-	// int offset = 2;
-	for (int y = 0; y < 10; y++){
+	int offset = 1;
+	for (int y = 0; y < 10; y++){		
 		for (int x = 0; x < 10; x++){
-			toReturn[count][0] = x * 3;
-			toReturn[count][1] = y * 3;
+			toReturn[count][0] = x * 1;
+			toReturn[count][1] = offset;
 			// if(y % 2 == 0){
 			// 	toReturn[count][0] += offset;
 			// } 
 			count++;
 		}
+		if(y%2 ==0){
+				offset +=1;
+			} else {
+				offset+=3;
+			}
 	}
 
-	for (int y = 0; y < 9; y++){
-		for (int x = 0; x < 9; x++){
-			toReturn[count][0] = x * 3 + 1.5;
-			toReturn[count][1] = y * 3 + 1.5;
-			// if(y % 2 == 0){
-			// 	toReturn[count][0] += offset;
-			// } 
-			count++;
-		}
-	}
+	// for (int y = 0; y < 9; y++){
+	// 	for (int x = 0; x < 9; x++){
+	// 		toReturn[count][0] = x * 3 + 1.5;
+	// 		toReturn[count][1] = y * 3 + 1.5;
+	// 		// if(y % 2 == 0){
+	// 		// 	toReturn[count][0] += offset;
+	// 		// } 
+	// 		count++;
+	// 	}
+	// }
 	//Noise
 	// for (int x = 0; x < 100; x++){
 	// 	//Randomise the data
@@ -53,7 +58,7 @@ int main(){
 	// }
 	//write code to output to a file
 	ofstream MyFile;
-	MyFile.open("QuincunxIdeal3.txt");
+	MyFile.open("DoubleHedgeIdeal3.txt");
 	for (int x = 0; x < count; x++){
 		MyFile << toReturn[x][0] << " " << toReturn[x][1] << endl;
 	}
