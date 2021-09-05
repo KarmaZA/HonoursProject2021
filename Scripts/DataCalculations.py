bf5848b835851f09a64bc6d709cf2468a2365f9a
Jonathon Everatt <everatt.jonathon@gmail.com>
2021-09-05 13:05:37 +0200

    Making sub images that plot correctly onto the data but still making it work properly

--- Scripts/DataCalculations.py
+++ Scripts/DataCalculations.py
@@ -13,52 +13,8 @@ import matplotlib.cm as cm
 import numpy as np
 from math import radians, cos, sin, atan2, degrees
 import random
-
-
-def convertPointsToInt(PointSet):
-    set_to_return= []
-    x_min, y_min, x_max, y_max = (400,400,-400,-400)
-    # x_min, y_min, x_max, y_max = (0,0, -0.0058, 0.006778)
-    for point in PointSet:
-        if point.x < x_min : x_min = point.x
-        if point.y < y_min : y_min = point.y
-        if point.x > x_max : x_max = point.x
-        if point.y > y_max : y_max = point.y
-    x_max = x_min + (-0.0058)
-    # x_min -= x_min
-    y_min = y_max - 0.00006778
-    # y_min -= y_min
-    print(x_min, y_min, x_max, y_max)
-    for point in PointSet:
-        if point.y > y_min: 
-            set_to_return.append(Point(point.x, point.y))
-            print("here")
-        else: 
-            print(point.y)
-        # if ((point.x > x_min) and (point.x < x_max)) and ((point.y > y_min) and (point.y < y_max)):
-        #     set_to_return.append(Point(point.x, point.y))
-        #     print("here")
-            
-    # delta_y = (y_max-y_min)/100        
-    print("A visual depictions of your orchard")
-    xs = [point.x for point in set_to_return]
-    ys = [point.y for point in set_to_return]
-    plt.scatter(xs,ys, color = 'black')
-    # plt.yticks(np.arange(y_min, y_max, delta_y))
-    plt.savefig('OrchardGraph.png')
-    plt.show()
-    x1s = [point.x for point in PointSet]
-    y1s = [point.y for point in PointSet]
-    plt.scatter(x1s,y1s, color = 'black')
-    plt.scatter(xs,ys, color = 'blue')
-    plt.show()
-    return MultiPoint(PointSet)
-    
-    # set_to_return.append(Point(0,0))
-    # delta_x, delta_y = (-PointSet[0].x, -PointSet[0].y)
-    # float_count = 10 **(len(str(PointSet[0].x).split('.')[1]))
-    
-    # for k in range(1, len(PointSet)):
+################### Distance Calculations
+# for k in range(1, len(PointSet)):
     #     # Point1 = Point(PointSet[k-1].x, PointSet[k-1].y)
     #     # Point2 = 
     #     # line_distance = LineString([PointSet[k-1], PointSet[k]])
@@ -76,21 +32,67 @@ def convertPointsToInt(PointSet):
     #     set_to_return.append(Point((PointSet[k].x + delta_x), (PointSet[k].y + delta_y)))
         
     #     set_to_return[k] = Point(int(set_to_return[k].x*float_count),int(set_to_return[k].y*float_count))
-    # print("xy delta")
-    # print(delta_x/len(PointSet))
-    # print(delta_y/len(PointSet))    
-    # xs = [point.x for point in set_to_return]
-    # ys = [point.y for point in set_to_return]
-    # plt.scatter(xs,ys, color = 'black')
-    # plt.show()
-    # return MultiPoint(set_to_return)
-
-
-# #Where point1 and2 are from PointSet. Returns distance in meters
+    
+    
+    # #Where point1 and2 are from PointSet. Returns distance in meters
 # def calcScaleUnitValues(point1,point2):
 #     loc1 = (point1.x, point1.y)
 #     loc2 = (point2.x, point2.y)
 #     return hs.haversine(loc1, loc2, unit=Unit.METERS)
+
+
+
+def convertPointsToInt(PointSet):
+    count = 0
+    print(len(PointSet))
+    x_min, y_min, x_max, y_max = (400,400,-400,-400)
+    for point in PointSet:
+        if point.x < x_min : x_min = point.x
+        if point.y < y_min : y_min = point.y
+        if point.x > x_max : x_max = point.x
+        if point.y > y_max : y_max = point.y
+        
+    max_image_number = int((len(PointSet)/100)+1)
+
+    delta_x = (x_max-x_min)/max_image_number
+    delta_y = (y_max-y_min)/max_image_number
+
+    for x in range (max_image_number):                                        
+        set_to_return= []        
+        # Set boundaries for edges of sub image
+        x_max = x_min + delta_x
+        y_max = y_min + delta_y
+        
+        print(x_min, y_min, x_max, y_max)
+        for point in PointSet:
+            # if (point.y > y_min) and (point.x > x_min):
+            #     if(point.y < y_max): 
+            #         set_to_return.append(Point(point.x, point.y))
+
+            if ((point.x > x_min) and (point.x < x_max)) and ((point.y > y_min) and (point.y < y_max)):
+                set_to_return.append(Point(point.x, point.y))
+                print("here")
+                
+        print("A visual depictions of your orchard")
+        xs = [point.x for point in set_to_return]
+        ys = [point.y for point in set_to_return]
+        plt.scatter(xs,ys, color = 'black')
+        # plt.yticks(np.arange(y_min, y_max, delta_y))
+        plt.savefig('OrchardGraph' + str(count) + '.png')
+        plt.show()
+        x1s = [point.x for point in PointSet]
+        y1s = [point.y for point in PointSet]
+        plt.scatter(x1s,y1s, color = 'black')
+        plt.scatter(xs,ys, color = 'blue')
+        plt.show()
+        
+        #Update the boundaries of the sub - image
+        x_min = x_max
+        y_min = y_max
+        count +=1 
+        
+
+
 
 def normaliseData(PointSet):
     dataset = KDTree(PointSet)
